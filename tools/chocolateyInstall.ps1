b4af0ca88175cd445fff9192e1141cfd1193af32
Pieter Joost van de Sande <pj@born2code.net>
2017-08-19 11:55:30 +0100

    update installation script to install v3.4.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 Install-ChocolateyZipPackage 'protoc' `
- 'https://github.com/google/protobuf/releases/download/v2.6.1/protoc-2.6.1-win32.zip' `
+ https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-win32.zip' `
  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
- -checksum 'b057f86ef83835010bb227eb2d82de04' -checksumType 'md5'
+ -checksum '49E7A8CD93660305DA12017179E25477' -checksumType 'md5'
